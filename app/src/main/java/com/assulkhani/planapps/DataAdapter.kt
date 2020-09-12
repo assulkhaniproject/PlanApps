c8a6844487f4da2491532106cd614852c5c639fa
assulkhaniproject <assulkhaniproject@gmail.com>
2020-09-12 09:20:06 +0700

    update logic model

--- app/src/main/java/com/assulkhani/planapps/DataAdapter.kt
+++ app/src/main/java/com/assulkhani/planapps/DataAdapter.kt
@@ -1,28 +1,45 @@
 package com.assulkhani.planapps
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_data.view.*
+import javax.security.auth.callback.Callback
 
-class DataAdapter () : RecyclerView.Adapter<DataAdapter.ViewHolder> () {
+class DataAdapter (val callback : Callback) : RecyclerView.Adapter<DataAdapter.ViewHolder> () {
 
     private var dataList : List<DataModel> = ArrayList()
     fun setData(data : List<DataModel>){
         this.dataList = data
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataAdapter.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.item_data, parent, false)
+        return ViewHolder(view, callback)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return dataList.size
     }
 
     override fun onBindViewHolder(holder: DataAdapter.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bind(dataList[position])
     }
 
-    class ViewHolder (itemView : View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder(itemView: View, val callback:Callback) : RecyclerView.ViewHolder(itemView) {
+        fun bind(data : DataModel){
+            itemView.tv_title.text = data.title
+            itemView.tv_desc.text = data.desc
+
+            itemView.setOnClickListener {
+                callback.onClick(data)
+            }
+        }
+
+        interface Callback {
+            fun onClick(data : DataModel)
+            
+        }
 
     }
 }
--- app/src/main/java/com/assulkhani/planapps/MainActivity.kt
+++ app/src/main/java/com/assulkhani/planapps/MainActivity.kt
@@ -2,11 +2,30 @@ package com.assulkhani.planapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DataAdapter.Callback{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var dataList = ArrayList<DataModel>()
+        dataList.add(DataModel(1, "Belajar Android", "Loren Ipsum Today"))
+        dataList.add(DataModel(1, "Belajar Android", "Loren Ipsum Today"))
+        dataList.add(DataModel(1, "Belajar Android", "Loren Ipsum Today"))
+        dataList.add(DataModel(1, "Belajar Android", "Loren Ipsum Today"))
+        dataList.add(DataModel(1, "Belajar Android", "Loren Ipsum Today"))
+
+        var dataAdapter = DataAdapter(this)
+        rv_data.apply {
+            var linearLayoutManager = LinearLayoutManager(context)
+            linearLayoutManager.orientation = LinearLayoutManager.VERTICAL
+            layoutManager = linearLayoutManager
+            adapter = dataAdapter
+        }
+        dataAdapter.setData(dataList)
+        dataAdapter.notifyDataSetChanged()
     }
 }
